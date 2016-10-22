308143b0110c1999d7c989b3d9e2049fc5055cb0
Peter Loos <peter.loos@gmx.de>
2016-10-22 14:12:37 +0200

    Minor Changes

--- Program.cpp
+++ Program.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 void main()
 {
-    long ticks = ::GetTickCount();
-
     EinsteinRiddleSolver solver;
+    cout << "Einstein Riddle:" << endl;
 
     // solver.Solve_BruteForce_01();  // runs too loooong
 
+    long ticks = ::GetTickCount();
     solver.Solve_BruteForce_02();
     ticks = ::GetTickCount() - ticks;
     cout << "[" <<  ticks << " msecs]" << endl;
